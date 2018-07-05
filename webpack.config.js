50e8429f130b9774f710de361a318a26a671bada
Jacob Schaefer <jacobschaefer12@gmail.com>
2018-07-05 15:59:53 -0700

    First commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-  entry: {
-    App: "./app/assets/scripts/App.js",
-    Vendor: "./app/assets/scripts/Vendor.js"
-  },
+  entry: "./app/assets/scripts/App.js",
   output: {
     path: path.resolve(__dirname, "./app/temp/scripts"),
-    filename: "[name].js"
+    filename: "App.js"
   },
   module: {
     loaders: [
